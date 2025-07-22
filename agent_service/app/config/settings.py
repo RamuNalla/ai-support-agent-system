363126d99fe04eaf4adaa36b2dc21f63a1c88d79
RamuNalla <nallaramu4321@gmail.com>
2025-07-22 22:23:05 +0530

    FAISS VectorDB Setup

--- agent_service/app/config/settings.py
+++ agent_service/app/config/settings.py
@@ -2,10 +2,12 @@ import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from dotenv import load_dotenv
 
-load_dotenv()
-class Settings(BaseSettings):
+load_dotenv()                           # Load variables from the .env file
+class Settings(BaseSettings):           # Declares the API KEY is a string and required
     APP_NAME: str = "AI Support Agent Service"
     GEMINI_API_KEY: str 
+
+    FAISS_INDEX_PATH: str = "agent_service/faiss_index.bin"         # expected path for the FAISS index file.
 
     model_config = SettingsConfigDict(env_file='.env', extra='ignore')
 
